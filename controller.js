60d49978ba55c2971ecb3810d174ec9a409775f2
thunderchunk <lucaswhawk@gmail.com>
2017-04-20 11:57:56 -0500

    initial upload

---
+++ controller.js
@@ -0,0 +1,12 @@
+// INITILIZE CONTROLLER
+// ============================================================
+angular.module("blankApp").controller("mainCtrl", function($scope, $state) {
+
+  // VARIABLES
+  // ============================================================
+$scope.test = "test";
+  // FUNCTIONS
+  // ============================================================
+
+
+});
